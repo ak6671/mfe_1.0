a897f4306d4360e0cb6c1215940b1f8d518f90bb
Ajith <ajak7770@gmail.com>
2026-06-08 20:10:39 +0530

    add yml config

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common.js");
+const moduleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require('../package.json');
+
+const domain_url = process.env.PRODUCTION_DOMAIN
+
+const prodConfig = {
+  mode: "production",
+  output:{
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new moduleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain_url}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies // ["react", "react-dom"] // ,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
